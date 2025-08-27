b7f2243c21afd00e87640d20222d8e7cbf7a4a3e
SaiPh3r <saianand750@gmail.com>
2025-08-27 21:19:30 +0530

    preorder , postorder , inorder

--- Trees/Build.cpp
+++ Trees/Build.cpp
@@ -30,6 +30,73 @@ Node* build_tree(){
     }
 }
 
+// Traversals
+
+// NLR - preorder traversal
+void preorderTraversal(Node* root){
+    // base case
+    if(root == NULL){
+        return;
+    }
+
+    //N
+    cout<<root->value<<" ";
+
+    // L
+    preorderTraversal(root->left);
+
+    //R
+    preorderTraversal(root->right);
+}
+
+// LNR-inorder
+void inorder(Node* root){
+    // base case
+    if(root == NULL){
+        return;
+    }
+    //L
+    inorder(root->left);
+
+    // N - Current
+    cout<<root->value<<" ";
+
+    //R
+    inorder(root->right);
+}
+
+// LRN - postorder
+
+void postorder(Node* root){
+    // base case
+    if(root == NULL){
+        return;
+    }
+    // L
+    postorder(root->left);
+
+    // R
+    postorder(root->right);
+
+    // N
+    cout<<root->value<<" ";
+
+}
+
 int main(){
+    // 10 20 30 -1 -1 40 -1 -1 50 -1 60 -1 -1
+    Node* root = build_tree();
+    cout<<"printing pre-order"<<endl;
+    preorderTraversal(root);
+    cout<<endl;
+
+    cout<<"printing inorder"<<endl;
+    inorder(root);
+    cout<<endl;
+
+    cout<<"printing post-order"<<endl;
+    postorder(root);
+    cout<<endl;
+
     return 0;
 }
