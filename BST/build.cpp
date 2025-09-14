0a74d3ba360d9af73fd2a15b810bdcd1f4e3037b
SaiPh3r <saianand750@gmail.com>
2025-09-14 18:50:59 +0530

    done build bst

--- BST/build.cpp
+++ BST/build.cpp
@@ -39,11 +39,31 @@ Node* createBst(Node* root){
         cout<<"enter the value for the node"<<endl;
         cin>>val;
     }
+    return root;
+
+}
+//nlr
+void preorder(Node* root ){
+    if(root ==  NULL){
+        return;
+    }
+    // n
+    cout<<root->data<<" ";
+
+    //l
+    preorder(root->left);
+
+    //r
+    preorder(root->right);
 
 }
 
 
 
 int main(){
+    Node* root = NULL;
+    root = createBst(root);
+    preorder(root);
     return 0; 
+
 }
