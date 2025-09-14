a3ea4878ad2f9a145292058c65089adf0fcceec8
SaiPh3r <saianand750@gmail.com>
2025-09-14 18:37:01 +0530

    post-burnout commit

---
+++ BST/build.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value){
+        data = value;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+Node* buildBst(Node* root , int value){
+    if(root == NULL){
+        root = new Node(value);
+        return root;
+    }
+
+    else if (value>root->data){
+        root->right = buildBst(root->right , value);
+    }else{
+        root->left = buildBst(root->left , value);
+    }
+
+    return root;
+}
+
+Node* createBst(Node* root){
+    cout<<"enter the value for the node: "<<endl;
+    int val;
+    cin>>val;
+
+    while(val!=-1){
+        root = buildBst(root , val);
+        cout<<"enter the value for the node"<<endl;
+        cin>>val;
+    }
+
+}
+
+
+
+int main(){
+    return 0; 
+}
